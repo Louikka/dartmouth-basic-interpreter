cdffdac3724a48a3c7f673beba3746e553f4d0a4
Louikka <lolikadimonaa@gmail.com>
2026-02-22 15:53:31 +0300

    scanner complete (barebones, at least)

---
+++ parser/scanner.go
@@ -0,0 +1,37 @@
+package parser
+
+import "fmt"
+
+type Scanner struct {
+	Input           string
+	CurrentPosition int
+}
+
+func (scanner *Scanner) Peek(pos int) (byte, error) {
+	strlen := len(scanner.Input)
+	newPos := scanner.CurrentPosition + pos
+
+	if newPos < 0 || newPos >= strlen {
+		return 0, fmt.Errorf("(E001) Scanner trying to access character that out of bounds (peeking at position %d in string with length %d).", newPos, strlen)
+	}
+
+	return scanner.Input[newPos], nil
+}
+
+func (scanner *Scanner) Next() (byte, error) {
+	scanner.CurrentPosition++
+	return scanner.Peek(0)
+}
+
+// Returns true if current position is greater or equal to length of the input; otherwise returns false.
+func (scanner *Scanner) IsEndOfString() bool {
+	strlen := len(scanner.Input)
+	return scanner.CurrentPosition >= strlen
+}
+
+func NewScanner(s string) *Scanner {
+	return &Scanner{
+		Input:           s,
+		CurrentPosition: 0,
+	}
+}
